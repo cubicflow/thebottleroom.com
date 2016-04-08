876ca8bf4ead4a925a8f9e53d884a065a1de8761
Matt Ellison <matt@cubicflow.com>
2016-04-07 22:14:35 -0700

    cart-bar styling
    span hide/show on medium
    other css

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,6 @@ $(document).foundation();
 
 $(function() {
 
-
   //
   // CF-IMGSWAP WITH CUSTOM OPTIONS
   //
@@ -70,6 +69,37 @@ $(function() {
     }
   });
 
+
+  ///////////
+  // SNIPCART BAR & SNIPCART SETTINGS
+  ///////////
+
+  const cartBar = document.querySelector('#cart-bar')
+  let itemsCount = 0;
+
+  const showBarIfNeeded = function(){
+
+    itemsCount = Snipcart.api.getItemsCount();
+    if (itemsCount > 0){
+      cartBar.classList.add('active');
+    } else {
+      cartBar.classList.remove('active');
+    }
+  }
+
+  Snipcart.subscribe('cart.ready', function() {
+    showBarIfNeeded();
+  });
+
+  Snipcart.subscribe('item.added', function (item) {
+    showBarIfNeeded();
+  });
+
+  Snipcart.subscribe('item.removed', function (item) {
+    showBarIfNeeded();
+  });
+
+  // CONFIG
   Snipcart.execute('config', 'show_continue_shopping', true);
   Snipcart.execute('config', 'allowed_provinces', [
     { country: 'US', provinces: ['CA'] },
