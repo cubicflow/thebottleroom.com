d8a7e7e255ecb8ab1cd4ee0d6a98eb9db454119e
BackdoorDan <d.anmcn.air@gmail.com>
2016-05-11 12:33:03 +0900

    change deploy log func to console.log

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -9,21 +9,17 @@ var TRAVIS        = minimist(process.argv.slice(2));
 gulp.task('deploy', function() {
 
   var conn = ftp.create({
-  host:     TRAVIS.FTP_HOST,
-  user:     TRAVIS.FTP_USER,
-  password: TRAVIS.FTP_PASSWORD,
-  parallel: 10,
-  log:      browserSync.notify
+    host:     TRAVIS.FTP_HOST,
+    user:     TRAVIS.FTP_USER,
+    password: TRAVIS.FTP_PASSWORD,
+    parallel: 10,
+    log:      console.log
   });
-
-  var globs = [
-    '_site/',
-  ];
 
 // using base = '.' will transfer everything to /public_html correctly
 // turn off buffering in gulp.src for best performance
 
-return gulp.src( globs, { base: '.', buffer: false } )
+return gulp.src( '_site/**/*', { base: '.', buffer: false } )
   .pipe( conn.newer( TRAVIS.FTP_DESTINATION ) ) // only upload newer files
   .pipe( conn.dest( TRAVIS.FTP_DESTINATION ) );
 });
